0811d439852d9ce936f13471660adee7885063a8
Tecnologia02 <tecnologia02@intranet.local>
2020-09-29 08:54:52 -0300

    Criptografando password

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb+srv://danielofaustino:Anl92br.@cluster0.izb07.gcp.mongodb.net/sky?retryWrites=true&w=majority', 
-{ useNewUrlParser: true,
+mongoose.connect('mongodb+srv://danielofaustino:Anl92br.@cluster0.izb07.gcp.mongodb.net/ski?retryWrites=true&w=majority', 
+{useNewUrlParser: true,
 useUnifiedTopology: true,
 useCreateIndex: true});
 
